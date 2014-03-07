5914d614bcdaf0dd2b7e91dc58ed8060d23d0a8c
Scott Gardner <sgardn04@gmail.com>
2014-03-07 17:12:47 -0500

    cleaning up model

--- src/js/models/square.js
+++ src/js/models/square.js
@@ -1,17 +1,9 @@
 var Square = Backbone.Model.extend({
 	initialize : function(options){
-		// console.log("initializing Square");
-		// console.log(options);
-		if(options){
-			this.decay = options.decay || 0;
-			this.piece = options.piece || "none";
-		}
+		options = options || {};	
+		this.decay = options.decay || 0;
+		this.piece = options.piece || "none";
 
-	},
-
-	defaults : {
-		decay : 0,
-		piece : "none"
 	}
-
+	
 });
